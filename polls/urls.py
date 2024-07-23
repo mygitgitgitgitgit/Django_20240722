4461a528e63e2bc729c162be0473d524e755fe3a
mygitgitgitgitgit <cherro0108@gmail.com>
2024-07-23 23:03:25 +0900

    tutorial04 polls/views.py까지 작성

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
+    # path에 꺽쇠 괄호를 사용하면 URL의 일부가 캡처되어 키워드 인수로 view 함수에 전송
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
