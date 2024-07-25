237ca670d8618fc157bd54118426984f45a196f1
mygitgitgitgitgit <cherro0108@gmail.com>
2024-07-26 00:17:49 +0900

    Tutorial 07 검색기능 추가전

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from .models import Question
-from .models import Choice
+from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    # 관리자 페이지에서 Form field의 순서지정
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    fieldsets = [
+        ("Main Content", {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes":["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+admin.site.register(Question, QuestionAdmin)
